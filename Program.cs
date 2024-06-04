a2eb1fb1efa8fe2e7c9b43304bb6ab5abc5df3bb
alexk <alexk@DESKTOP-QL7PNT4>
2024-06-04 16:28:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Assignment_5._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NaturalNumbers(10);
+
+            void NaturalNumbers(int n) 
+            {
+                if (n == 1)
+                {
+                    Console.Write(n);
+                    
+                }
+                else 
+                { 
+                    NaturalNumbers(n - 1);
+                    Console.Write(" " + n);
+                }
+               
+
+                
+            }
+        }
+    }
+}
